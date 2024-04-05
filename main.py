1064abe5fbcf53f111787a31f51380ca9be980c9
jakedwyer <mjdwyer@gmail.com>
2024-04-05 20:16:38 +0000

    Update Selenium WebDriver options

--- main.py
+++ main.py
@@ -111,7 +111,7 @@ def fetch_list_members(list_id):
 # Initialize Selenium WebDriver
 def init_driver():
     options = webdriver.ChromeOptions()
-    # options.add_argument("--headless")
+    options.add_argument("--headless")
     options.add_argument("--disable-gpu")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
