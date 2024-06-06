7dcfa6bbf0205129f80325553e8215f72a46e895
jakedwyer <mjdwyer@gmail.com>
2024-06-06 15:49:42 +0000

    Refactor code for improved performance and readability

--- main.py
+++ main.py
@@ -5,7 +5,6 @@ from utils.logging_setup import setup_logging
 from twitter.twitter import fetch_list_members
 from scraping.scraping import init_driver, load_cookies, get_following
 
-
 def fetch_records_from_airtable(table_id, headers):
     base_id = 'appYCZWcmNBXB2uUS'
     url = f"https://api.airtable.com/v0/{base_id}/{table_id}"
@@ -27,49 +26,33 @@ def fetch_records_from_airtable(table_id, headers):
             return []
     return records
 
+def batch_request(url, headers, records, method):
+    for i in range(0, len(records), 10):
+        batch = records[i:i+10]
+        response = method(url, headers=headers, json={"records": batch})
+        if response.status_code in [200, 201]:
+            logging.info(f"{method.__name__.capitalize()} {len(batch)} entries in the {url.split('/')[-1]} table.")
+        else:
+            logging.error(f"Failed to {method.__name__} entries in {url.split('/')[-1]} table. Status code: {response.status_code}")
+            logging.error(f"Response content: {response.content}")
+
 def update_airtable_records(records, table_id, headers):
     base_id = 'appYCZWcmNBXB2uUS'
     url = f"https://api.airtable.com/v0/{base_id}/{table_id}"
-    
-    # Fetch existing records to verify record IDs
-    existing_records = fetch_records_from_airtable(table_id, headers)
-    existing_record_ids = {record['id'] for record in existing_records}
-    
-    for i in range(0, len(records), 10):
-        batch = records[i:i+10]
-        
-        # Filter out records with non-existing IDs
-        valid_batch = [record for record in batch if record['id'] in existing_record_ids]
-        
-        if not valid_batch:
-            logging.error(f"No valid records to update in batch: {batch}")
-            continue
-        
-        response = requests.patch(url, headers=headers, json={"records": valid_batch})
-        if response.status_code == 200:
-            logging.info(f"Updated {len(valid_batch)} entries in the {table_id} table.")
-        else:
-            logging.error(f"Failed to update entries in {table_id} table. Status code: {response.status_code}")
-            logging.error(f"Response content: {response.content}")
+    batch_request(url, headers, records, requests.patch)
 
 def create_airtable_records(records, table_id, headers):
     base_id = 'appYCZWcmNBXB2uUS'
     url = f"https://api.airtable.com/v0/{base_id}/{table_id}"
-    for i in range(0, len(records), 10):
-        batch = records[i:i+10]
-        response = requests.post(url, headers=headers, json={"records": batch})
-        if response.status_code == 200:
-            logging.info(f"Created {len(batch)} entries in the {table_id} table.")
-        else:
-            logging.error(f"Failed to create entries in {table_id} table. Status code: {response.status_code}")
-            logging.error(f"Response content: {response.content}")
+    batch_request(url, headers, records, requests.post)
 
-def fetch_and_update_accounts(usernames, headers, existing_usernames):
-    new_entries = [{"fields": {"Username": username}} for username in usernames if username not in existing_usernames]
+def fetch_and_update_accounts(usernames, headers, accounts_usernames):
+    new_entries = [{"fields": {"Username": username}} for username in usernames if username not in accounts_usernames]
     if new_entries:
         create_airtable_records(new_entries, 'tblJCXhcrCxDUJR3F', headers)
-    updated_records = fetch_records_from_airtable('tblJCXhcrCxDUJR3F', headers)
-    return {record['fields']['Username']: record['id'] for record in updated_records if 'Username' in record['fields']}
+        updated_records = fetch_records_from_airtable('tblJCXhcrCxDUJR3F', headers)
+        accounts_usernames.update({record['fields']['Username']: record['id'] for record in updated_records if 'Username' in record['fields']})
+    return accounts_usernames
 
 def fetch_existing_follows(record_id, headers):
     base_id = 'appYCZWcmNBXB2uUS'
@@ -82,15 +65,15 @@ def fetch_existing_follows(record_id, headers):
         logging.error(f"Response content: {response.content}")
         return set()
 
-def process_user(username, follower_record_id, driver, headers, existing_usernames):
+def process_user(username, follower_record_id, driver, headers, followers_usernames, accounts_usernames):
     existing_follows_usernames = fetch_existing_follows(follower_record_id, headers)
     new_follows_usernames = get_following(driver, username, existing_follows_usernames)
     all_follows_usernames = existing_follows_usernames.union(new_follows_usernames)
 
-    existing_usernames = fetch_and_update_accounts(all_follows_usernames, headers, existing_usernames)
+    accounts_usernames = fetch_and_update_accounts(all_follows_usernames, headers, accounts_usernames)
 
     # Map the followed usernames to their record IDs
-    followed_account_ids = [existing_usernames[username] for username in all_follows_usernames if username in existing_usernames]
+    followed_account_ids = [accounts_usernames[username] for username in all_follows_usernames if username in accounts_usernames]
 
     # Update Follower's "Account" field with the full list of account record IDs they follow
     follower_update = {
@@ -111,28 +94,30 @@ def main():
         return
 
     existing_followers = fetch_records_from_airtable('tbl7bEfNVnCEQvUkT', headers)
-    existing_usernames = {record['fields']['Username']: record['id'] for record in existing_followers if 'Username' in record['fields']}
-    new_followers = [{"fields": {"Account ID": member['id'], "Username": member['username']}} for member in list_members if member['username'] not in existing_usernames]
+    followers_usernames = {record['fields']['Username']: record['id'] for record in existing_followers if 'Username' in record['fields']}
+    
+    new_followers = [{"fields": {"Account ID": member['id'], "Username": member['username']}} for member in list_members if member['username'] not in followers_usernames]
     create_airtable_records(new_followers, 'tbl7bEfNVnCEQvUkT', headers)
+    
     updated_followers = fetch_records_from_airtable('tbl7bEfNVnCEQvUkT', headers)
-    existing_usernames.update({record['fields']['Username']: record['id'] for record in updated_followers if 'Username' in record['fields']})
+    followers_usernames.update({record['fields']['Username']: record['id'] for record in updated_followers if 'Username' in record['fields']})
+    
     existing_accounts = fetch_records_from_airtable('tblJCXhcrCxDUJR3F', headers)
-    existing_usernames.update({record['fields']['Username']: record['id'] for record in existing_accounts if 'Username' in record['fields']})
+    accounts_usernames = {record['fields']['Username']: record['id'] for record in existing_accounts if 'Username' in record['fields']}
 
     driver = init_driver()
     load_cookies(driver, env_vars['cookie_path'])
 
     for member in list_members:
         username = member['username']
-        record_id = existing_usernames.get(username)
+        record_id = followers_usernames.get(username)
         if record_id:
             try:
-                process_user(username, record_id, driver, headers, existing_usernames)
+                process_user(username, record_id, driver, headers, followers_usernames, accounts_usernames)
             except Exception as e:
                 logging.error(f"Error processing {username}: {e}")
 
     driver.quit()
-
 
 if __name__ == "__main__":
     main()
