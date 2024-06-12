fd56b3358d1798dd80e3aa683ea20e9d549e1d08
jakedwyer <mjdwyer@gmail.com>
2024-06-12 20:49:00 +0000

    chore: Update logging setup to include file output

--- utils/logging_setup.py
+++ utils/logging_setup.py
@@ -1,9 +1,13 @@
 import logging
+import os
 
 def setup_logging():
+    log_file_path = os.path.join(os.path.dirname(__file__), '../app.log')
     logging.basicConfig(
         level=logging.INFO,
         format='%(asctime)s - %(levelname)s - %(message)s',
-        filename='app.log',
-        filemode='a'
+        handlers=[
+            logging.FileHandler(log_file_path),
+            logging.StreamHandler()
+        ]
     )
